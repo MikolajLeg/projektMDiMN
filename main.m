e7fb73c512ba0c6277bd10bfd1407f5ba64f28fe
Piotr Sawicki <piotrsawicki13@gmail.com>
2022-05-08 20:43:55 +0200

    zmiany i dodaje gitignore

--- main.m
+++ main.m
@@ -1,20 +1,20 @@
-clear, close all
+clear, clc,
+close all
 
 img = imread("gwiazdy.PNG");
 %imshow(img)
 
 
-
-
 img = img(:,:,1);
 %figure,imshow(img)
-surf(img)
+%surf(img)
 
 len =size(img,2);
 heigh = size(img,1);
+macierz = double(img);
+macierz = interp2(macierz);
 
-macierz = interp2(img);
 %figure,imshow(img)
-figure,imshow(macierz)
+%figure,imshow(macierz)
 img = img-10;
 surf(macierz,"LineStyle","none",FaceColor="interp")
