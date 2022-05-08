eebef8d852222eea05583acd112033594b53b99d
Piotr Sawicki <piotrsawicki13@gmail.com>
2022-05-08 20:52:04 +0200

    mala zmiana w main

--- main.m
+++ main.m
@@ -6,15 +6,13 @@ img = imread("gwiazdy.PNG");
 
 
 img = img(:,:,1);
-%figure,imshow(img)
-%surf(img)
+
 
 len =size(img,2);
 heigh = size(img,1);
 macierz = double(img);
 macierz = interp2(macierz);
 
-%figure,imshow(img)
-%figure,imshow(macierz)
 img = img-10;
 surf(macierz,"LineStyle","none",FaceColor="interp")
+colormap('gray');
