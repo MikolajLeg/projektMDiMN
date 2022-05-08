f042af882428dd8524b8a77fde52fa31ecc23b16
Piotr Sawicki <piotrsawicki13@gmail.com>
2022-05-08 21:09:03 +0200

    zmiany

--- main.m
+++ main.m
@@ -11,8 +11,8 @@ img = img(:,:,1);
 len =size(img,2);
 heigh = size(img,1);
 macierz = double(img);
-macierz = interp2(macierz);
+macierz = interp2(macierz)-20;
 
-img = img-10;
+
 surf(macierz,"LineStyle","none",FaceColor="interp")
 colormap('gray');
