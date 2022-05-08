8fbcbda07f76f48d9bd3f5f34a306a8ce4c5b991
Piotr Sawicki <piotrsawicki13@gmail.com>
2022-05-08 20:34:24 +0200

    blebleble

--- main.m
+++ main.m
@@ -1,20 +1,20 @@
 clear, close all
 
-img = imread('gwiazdy.png');
-imshow(img)
+img = imread("gwiazdy.PNG");
+%imshow(img)
 
 
 
 
 img = img(:,:,1);
-figure,imshow(img)
+%figure,imshow(img)
 surf(img)
 
 len =size(img,2);
 heigh = size(img,1);
 
 macierz = interp2(img);
-figure,imshow(img)
+%figure,imshow(img)
 figure,imshow(macierz)
 img = img-10;
-surf(macierz)
+surf(macierz,"LineStyle","none",FaceColor="interp")
