f5efdd19696a061c8e7d8486e2a189d3de47a67d
Oliwier <oliwierkniazewski@gmail.com>
2022-05-15 14:00:19 +0200

    Liczenie pola gwiazd do pola obrazu przy pomocy Monte Carlo

--- main.m
+++ main.m
@@ -8,10 +8,15 @@ img1 = imbinarize(img);
 img1 =~ img1;
 figure, imshow(img)
 figure, imshow(img1)
-B = bwboundaries(img1);
-text(10,10,strcat('\color{green}Objects Found:',num2str(length(B))));
-hold on
-for k = 1:length(B)
-    boundary = B{k};
-    plot(boundary(:,2), boundary(:,1), 'g', 'LineWidth', 0.3);
-end
+%B = bwboundaries(img1);
+%text(10,10,strcat('\color{green}Objects Found:',num2str(length(B))));
+%hold on
+%for k = 1:length(B)
+    %boundary = B{k};
+    %plot(boundary(:,2), boundary(:,1), 'g', 'LineWidth', 0.3);
+%end
+
+%   Strzelanie i sprawdzanie czy punkt trafil na gwiazde
+
+pole = liczenie_pola_gwiazd(100000,2181,2500,img1);
+fprintf("Pole gwiazd w jednostkach^2 = %d", pole)
