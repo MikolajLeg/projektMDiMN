fdc14a9d1f21032feec55a705ddf129820c9a165
Piotr Sawicki <piotrsawicki13@gmail.com>
2022-05-24 20:54:25 +0200

    zmiany

--- main.m
+++ main.m
@@ -1,18 +1,15 @@
-clear, clc,
-close all
+clear, clc, close all
 
 img = imread("gwiazdy.PNG");
-%imshow(img)
-
+figure, imshow(img)
 
 img = img(:,:,1);
-
-
 len =size(img,2);
 heigh = size(img,1);
 macierz = double(img);
 macierz = interp2(macierz)-20;
 
-
+figure
 surf(macierz,"LineStyle","none",FaceColor="interp")
 colormap('gray');
+
