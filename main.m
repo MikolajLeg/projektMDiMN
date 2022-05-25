82c03c9a81812c5ad173bd0905985edf83afffc5
Piotr Sawicki <piotrsawicki13@gmail.com>
2022-05-25 17:50:10 +0200

    zmiany

--- main.m
+++ main.m
@@ -1,13 +1,13 @@
 clear, clc, close all
 
-img = imread("gwiazdy.PNG");
+img = imread("lepsze_zdjecie.jpg");
 figure, imshow(img)
 
 img = img(:,:,1);
 len =size(img,2);
 heigh = size(img,1);
 macierz = double(img);
-macierz = interp2(macierz)-20;
+macierz = interp2(macierz);
 
 figure
 surf(macierz,"LineStyle","none",FaceColor="interp")
